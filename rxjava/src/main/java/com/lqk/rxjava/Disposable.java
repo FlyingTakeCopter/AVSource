9531faa05e7b102c86e8c934dba70d6a2792de3a
FlyingTakeCopter <2402495702@qq.com>
2021-09-29 18:33:24 +0800

    add rx

--- rxjava/src/main/java/com/lqk/rxjava/Disposable.java
+++ rxjava/src/main/java/com/lqk/rxjava/Disposable.java
@@ -1,6 +1,12 @@
-package com.lqk.rxjava;/**
-* @创建者 lqk
-* @创建时间 2021/9/29 下午4:35
-* @描述 佛祖保佑此文件没有BUG
-*/public interface Disposable {
+package com.lqk.rxjava;
+
+/**
+ * @创建者 lqk
+ * @创建时间 2021/9/29 下午4:35
+ * @描述 佛祖保佑此文件没有BUG
+ */
+public interface Disposable<R> {
+    void dispose(boolean bool);
+
+    boolean isDisposable();
 }
